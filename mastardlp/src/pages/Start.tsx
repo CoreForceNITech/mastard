fbac2d855b927c943ba3b9e10c2f67775841aa6d
schezo817 <akito4440@gmail.com>
2022-07-11 17:23:27 +0900

    ver0.0.3 mastardで学ぶ・採用する仮完了

--- mastardlp/src/pages/Start.tsx
+++ mastardlp/src/pages/Start.tsx
@@ -5,46 +5,86 @@ import {
     Image,
     Stack,
     Text,
-    useBreakpointValue,
+    Container,
+    Box,
 } from '@chakra-ui/react';
 
 export function Start() {
     return (
-        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-            <Flex p={8} flex={1} align={'center'} justify={'center'}>
-                <Stack spacing={6} w={'full'} maxW={'lg'}>
-                    <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-                        <br />{' '}
-                        <Text color={'black'} as={'span'}>
-                            最先端知識のインプットと専門人材の採用をお手軽な値段で両立
-                        </Text>{' '}
-                    </Heading>
-                    <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-                        理系の学問を学ぶ大学院生とのマンツーマン講義で、最先端の知識を簡単アップデート
-                    </Text>
-                    <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-                        <Button
-                            rounded={'full'}
-                            bg={'blue.400'}
-                            color={'white'}
-                            _hover={{
-                                bg: 'blue.500',
-                            }}>
-                            デモに登録する
-                        </Button>
+        <Container maxW={'7xl'} p="12">
+            <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+                <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                    <Stack spacing={6} w={'full'} maxW={'lg'}>
+                        <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                            <br />{' '}
+                            <Text color={'black'} as={'span'}>
+                                最先端知識のインプットと専門人材の採用をお手軽な値段で両立
+                            </Text>{' '}
+                        </Heading>
+                        <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+                            理系の学問を学ぶ大学院生とのマンツーマン講義で、最先端の知識を簡単アップデート
+                        </Text>
+                        <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+                            <Button
+                                rounded={'full'}
+                                bg={'blue.400'}
+                                color={'white'}
+                                _hover={{
+                                    bg: 'blue.500',
+                                }}>
+                                デモに登録する
+                            </Button>
+                        </Stack>
                     </Stack>
+                </Flex>
+                <Flex flex={1}>
+                    <Image
+                        alt={'Login Image'}
+                        objectFit={'cover'}
+                        src={
+                            'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                        }
+                    />
+                </Flex>
+            </Stack>
+            <Box border='2px' borderColor='blue.400'>
+                <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+                    <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                        <Stack spacing={6} w={'full'} maxW={'lg'}>
+                            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                                <br />{' '}
+                                <Text color={'blue.400'} as={'span'}>
+                                    mastardで学ぶ・採用する
+                                </Text>{' '}
+                            </Heading>
+                            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+                                私たちmastardはまだ始まったばかり。詳しく知りたい方は、ぜひお問合わせフォームへ。
+                            </Text>
+                            <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+                                <Button
+                                    rounded={'full'}
+                                    bg={'yellow.400'}
+                                    color={'black'}
+                                    _hover={{
+                                        bg: 'blue.500',
+                                    }}>
+                                    お問合せフォームへ
+                                </Button>
+                            </Stack>
+                        </Stack>
+                    </Flex>
+                    <Flex flex={1}>
+                        <Image
+                            alt={'Login Image'}
+                            objectFit={'cover'}
+                            src={
+                                'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                            }
+                        />
+                    </Flex>
                 </Stack>
-            </Flex>
-            <Flex flex={1}>
-                <Image
-                    alt={'Login Image'}
-                    objectFit={'cover'}
-                    src={
-                        'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-                    }
-                />
-            </Flex>
-        </Stack>
+            </Box>
+        </Container>
     );
 }
 
