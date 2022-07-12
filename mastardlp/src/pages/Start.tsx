01159889ee3a50abd56e9945df29f8ac0b141941
schezo817 <akito4440@gmail.com>
2022-07-12 16:31:48 +0900

    ver0.0.5 ページ下部にデバック用遷移ボタンの設定

--- mastardlp/src/pages/Start.tsx
+++ mastardlp/src/pages/Start.tsx
@@ -8,6 +8,7 @@ import {
     Container,
     Box,
 } from '@chakra-ui/react';
+import { Link } from "react-router-dom";
 
 export function Start() {
     return (
@@ -84,6 +85,10 @@ export function Start() {
                     </Flex>
                 </Stack>
             </Box>
+            <Link to={`/Business/`}>法人向けページ</Link>
+            <Link to={`/Coach/`}>大学院生向けページ</Link>
+            <Link to={`/Curriculum/`}>カリキュラム・プラン</Link>
+            <Link to={`/AboutCompany/`}>会社概要</Link>
         </Container>
     );
 }
