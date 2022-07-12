b8e509556d898507e56fb15910a071e40b0d2c6b
schezo817 <akito4440@gmail.com>
2022-07-12 16:21:59 +0900

    ver0.0.4 各ページの作成

--- mastardlp/src/App.tsx
+++ mastardlp/src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Start from './pages/Start';
+import Business from './pages/Business';
+import Coach from './pages/Coach';
+import Curriculum from './pages/Curriculum';
+import AboutCompany from './pages/AboutCompany';
 
 function App() {
   return (
     <Routes>
       <Route path={`/`} element={<Start />} />
+      <Route path={`/business/`} element={<Business />} />
+      <Route path={`/coach/`} element={<Coach />} />
+      <Route path={`/curriculum/`} element={<Curriculum />} />
+      <Route path={`/aboutcompany/`} element={<AboutCompany />} />
     </Routes>
   );
 }
