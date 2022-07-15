aaa2b56c92fdb333918c691b4e6d8e519ae78791
schezo817 <akito4440@gmail.com>
2022-07-15 13:55:23 +0900

    ver0.0.12 manabaleのロゴ追加

--- mastardlp/src/pages/Start.tsx
+++ mastardlp/src/pages/Start.tsx
@@ -9,6 +9,7 @@ import {
     Box,
 } from '@chakra-ui/react';
 import { Link } from "react-router-dom";
+import logo from '../images/manabale_logo.png';
 
 export function Start() {
     return (
@@ -40,10 +41,10 @@ export function Start() {
                 </Flex>
                 <Flex flex={1}>
                     <Image
-                        alt={'Login Image'}
+                        alt={'Manabale Image'}
                         objectFit={'cover'}
                         src={
-                            'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                            logo
                         }
                     />
                 </Flex>
