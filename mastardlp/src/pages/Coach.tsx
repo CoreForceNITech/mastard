8778fecab5eec6d16b178dccffe6e7d0a571d977
schezo817 <akito4440@gmail.com>
2022-07-17 16:42:43 +0900

    ver0.0.32 大学院生むけページの作成

--- mastardlp/src/pages/Coach.tsx
+++ mastardlp/src/pages/Coach.tsx
@@ -11,6 +11,7 @@ import {
 import { Header } from '../components/Header';
 import { Recruit } from "../components/Recruit";
 import { Footer } from '../components/Footer';
+import { DemoRegisterButton } from "../components/DemoRegisterButton";
 
 export function Coach() {
     return (
@@ -18,39 +19,63 @@ export function Coach() {
             <div className="recruit">
                 <Header />
             </div>
+
             <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
                 <Flex p={8} flex={1} align={'center'} justify={'center'}>
                     <Stack spacing={6} w={'full'} maxW={'lg'}>
                         <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
                             <br />{' '}
                             <Text color={'black'} as={'span'}>
-                                最先端知識のインプットと専門人材の採用をお手軽な値段で両立
+                                社会人に最先端の知識を説明してお金を稼ぎませんか?
                             </Text>{' '}
                         </Heading>
                         <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-                            理系の学問を学ぶ大学院生とのマンツーマン講義で、最先端の知識を簡単アップデート
+                            社会人へのマンツーマン講義で、お金を稼ぎませんか?
                         </Text>
-                        <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-                            <Button
-                                rounded={'full'}
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}>
-                                デモに登録する
-                            </Button>
+                        <Stack align={'center'} justify={'center'} direction={{ base: 'column', md: 'row' }} spacing={4}>
+                            <div>
+                                <DemoRegisterButton />
+                            </div>
                         </Stack>
                     </Stack>
                 </Flex>
-                <Flex flex={1}>
-                    <Image
-                        alt={'Login Image'}
-                        objectFit={'cover'}
-                        src={
-                            'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-                        }
-                    />
+            </Stack>
+
+            <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+                <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                    <Stack spacing={6} w={'full'} maxW={'lg'}>
+                        <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                            <br />{' '}
+                            <Text color={'black'} as={'span'}>
+                                mastardだからできること
+                            </Text>{' '}
+                        </Heading>
+                        <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+                            社会人へのマンツーマン講義だからこそ、ただお金を稼ぐだけで終わらない体験価値がある。
+                        </Text>
+                        <Stack align={'center'} justify={'center'} direction={{ base: 'column', md: 'row' }} spacing={4}>
+                            <div>
+                                <DemoRegisterButton />
+                            </div>
+                        </Stack>
+                    </Stack>
+                </Flex>
+            </Stack>
+            <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+                <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                    <Stack spacing={6} w={'full'} maxW={'lg'}>
+                        <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                            <br />{' '}
+                            <Text color={'black'} as={'span'}>
+                                mastardの講師からのコメント
+                            </Text>{' '}
+                        </Heading>
+                        <Stack align={'center'} justify={'center'} direction={{ base: 'column', md: 'row' }} spacing={4}>
+                            <div>
+                                <DemoRegisterButton />
+                            </div>
+                        </Stack>
+                    </Stack>
                 </Flex>
             </Stack>
             <div className="recruit">
