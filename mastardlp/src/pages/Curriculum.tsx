4e71f86c460df5d1a1f6c09acb202bd62c03fea5
FunabikiRikuto <rikuto27gifu@gmail.com>
2022-07-18 10:17:42 +0900

    ver.0.0.33CurriculumにRecruit, Footerの追加

--- mastardlp/src/pages/Curriculum.tsx
+++ mastardlp/src/pages/Curriculum.tsx
@@ -10,7 +10,10 @@ import {
     Avatar,
     Wrap,
 } from '@chakra-ui/react';
+import { Recruit } from "../components/Recruit";
 import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+import { InquiryButton } from '../components/InquiryButton';
 
 export function Curriculum() {
     return (
@@ -124,45 +127,12 @@ export function Curriculum() {
                 </Box>
             </Wrap>
 
-
-
-            <Box border='2px' borderColor='blue.400'>
-                <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-                    <Flex p={8} flex={1} align={'center'} justify={'center'}>
-                        <Stack spacing={6} w={'full'} maxW={'lg'}>
-                            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-                                <br />{' '}
-                                <Text color={'blue.400'} as={'span'}>
-                                    mastardで学ぶ・採用する
-                                </Text>{' '}
-                            </Heading>
-                            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
-                                私たちmastardはまだ始まったばかり。詳しく知りたい方は、ぜひお問合わせフォームへ。
-                            </Text>
-                            <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-                                <Button
-                                    rounded={'full'}
-                                    bg={'yellow.400'}
-                                    color={'black'}
-                                    _hover={{
-                                        bg: 'blue.500',
-                                    }}>
-                                    お問合せフォームへ
-                                </Button>
-                            </Stack>
-                        </Stack>
-                    </Flex>
-                    <Flex flex={1}>
-                        <Image
-                            alt={'Login Image'}
-                            objectFit={'cover'}
-                            src={
-                                'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-                            }
-                        />
-                    </Flex>
-                </Stack>
-            </Box>
+            <div className="recruit">
+                <Recruit />
+            </div>
+            <div className="footer">
+                <Footer />
+            </div>
         </Container>
     );
 }
