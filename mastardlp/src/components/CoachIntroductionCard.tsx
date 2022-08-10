8752ac7aef49065f60c3be713513926622000cc3
FunabikiRikuto <rikuto27gifu@gmail.com>
2022-08-10 21:25:42 +0900

    講師紹介カード画像の変更

--- mastardlp/src/components/CoachIntroductionCard.tsx
+++ mastardlp/src/components/CoachIntroductionCard.tsx
@@ -7,6 +7,8 @@ import {
     Box,
     Avatar,
 } from '@chakra-ui/react';
+import human from '../images/human.png';
+import natural_language_processing from '../images/natural_language_processing.jpg';
 
 export const CoachIntroductionCard: FC = () => {
     return (
@@ -14,8 +16,11 @@ export const CoachIntroductionCard: FC = () => {
             <Box h="480px" w="300px" bg="#c9ffe8" boxShadow="xl" rounded="xl" p="10" overflow="hidden">
                 <Box h="210px" mt="-6" mx="-6" rounded="xl" pos="relative">
                     <Image
-                        // src={img.src}
-                        fallbackSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwyqz9CAQtweBZh-kkQekbVQBqq5kE74Xmaw&usqp=CAU/280x210"
+                        alt={'natural_language_processing'}
+                        objectFit={'contain'}
+                        src={
+                            natural_language_processing
+                        }
                     />
                 </Box>
 
@@ -33,17 +38,19 @@ export const CoachIntroductionCard: FC = () => {
                     <Box h="100px" overflow="auto">
                         <Text color="gray.500" fontSize="sm">
                             自然言語処理やLinked Open Data、行動センシング技術などのAIの要素技術を使って、人々のコラボレーションを支援する技術を研究・開発しています。
-                            自然言語処理やLinked Open Data、行動センシング技術などのAIの要素技術を使って、人々のコラボレーションを支援する技術を研究・開発しています。
                         </Text>
                     </Box>
                 </Stack>
                 <Stack mt="6" direction="row" spacing="4" align="center">
                     <Avatar
-                        src="http://www.srmt.nitech.ac.jp/img/member/siramatu2016.png"
+                        objectFit={'contain'}
+                        src={
+                            human
+                        }
                     />
                     <Stack direction="column" spacing="0" fontSize="sm">
-                        <Text fontWeight="600">白松 俊</Text>
-                        <Text color="gray.500">名古屋工業大学　教授</Text>
+                        <Text fontWeight="600">船曳陸斗</Text>
+                        <Text color="gray.500">名古屋工業大学　学部3年生</Text>
                     </Stack>
                 </Stack>
             </Box>
